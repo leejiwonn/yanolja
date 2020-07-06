a987e9b2eb91e9c6f59aa9da73867679ef117c91
leejiwonn <bbongwa@naver.com>
2020-07-07 02:40:33 +0900

    test animation

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@
 
   var controller = new ScrollMagic.Controller();
 
-  var tweenP1 = gsap.fromTo(".p1-logo", { y: 100, opacity: 0, duration: 1, ease: "sine.inOut" }, { y: 0, opacity: 1, duration: 0.8, ease: "sine.inOut" });
+  var tweenP1 = gsap.timeline()
+  .fromTo(".p1-logo", { y: 100, opacity: 0, duration: 0.8, ease: "sine.inOut" }, { y: 0, opacity: 1, duration: 0.5, ease: "sine.inOut"})
+  .to(".p1-logo", {y: 30, duration: 0.8, ease: "sine.inOut", repeat: -1, yoyo: true});
 
   var sceneP1 = new ScrollMagic.Scene({
     triggerElement: ".trigger1",
