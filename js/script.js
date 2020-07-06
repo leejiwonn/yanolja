f3836fdb3e360f972e2a47ae6eedaea0b0424b04
leejiwonn <bbongwa@naver.com>
2020-07-07 03:33:43 +0900

    애니메이션 적용중

--- js/script.js
+++ js/script.js
@@ -7,13 +7,28 @@
   .to(".p1-logo", {y: 30, duration: 0.8, ease: "sine.inOut", repeat: -1, yoyo: true});
 
   var sceneP1 = new ScrollMagic.Scene({
-    triggerElement: ".trigger1",
+    triggerElement: ".p1-trigger-1",
     triggerhook: 0.8
   })
-    .setTween(tweenP1)
-    .addTo(controller)
-    .addIndicators({
-      name: "1"
-    });
+  .setTween(tweenP1)
+  .addTo(controller)
+  .addIndicators({
+    name: "1"
+  });
+
+  var tweenP2 = gsap.timeline()
+  .fromTo(".p1-wave", { y: 0, duration: 0.8, ease: "sine.inOut" }, { y: -50, duration: 0.5, ease: "sine.inOut"}, "with")
+  .fromTo(".illurst", { y: 30, duration: 0.8, ease: "sine.inOut" }, { y: -50, duration: 0.5, ease: "sine.inOut", delay: 0.2}, "with")
+  .fromTo(".ill-back", { x: 0, duration: 0.8, ease: "sine.inOut" }, { x: -10, duration: 0.5, ease: "sine.inOut", delay: 0.5}, "with");
+
+  var sceneP2 = new ScrollMagic.Scene({
+    triggerElement: ".p1-trigger-2",
+    triggerhook: 0.8
+  })
+  .setTween(tweenP2)
+  .addTo(controller)
+  .addIndicators({
+    name: "2"
+  });
 
 })();
